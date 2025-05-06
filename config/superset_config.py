50ec62f64215fa6557bf87fa07d1f0d4c3ae0221
adamsmec <your-adam,mosam@smec.com>
2025-05-06 14:39:04 +0200

    Enable Superset embedding

--- config/superset_config.py
+++ config/superset_config.py
@@ -2,6 +2,14 @@ import os
 
 FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
+    "EMBEDDED_SUPERSET": True,
+}
+
+CORS_OPTIONS = {
+  'supports_credentials': True,
+  'allow_headers': ['*'],
+  'resources':['*'],
+  'origins': ['http://localhost:8088', 'http://localhost:8888', "http://localhost:3000"]
 }
 
 ENABLE_PROXY_FIX = True
